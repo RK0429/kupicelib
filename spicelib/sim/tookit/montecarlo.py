d722107b32635f8fb1cca3c1c774883679786430
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-02-27 15:28:18 +0900

    Update montecarlo.py

--- spicelib/sim/tookit/montecarlo.py
+++ spicelib/sim/tookit/montecarlo.py
@@ -20,13 +20,13 @@
 
 import logging
 import random
-from typing import Union, Callable, Type
+from typing import Callable, Optional, Type, Union
+
+from ...log.logfile_data import LogfileData
+from ..process_callback import ProcessCallback
+from .tolerance_deviations import ComponentDeviation, DeviationType, ToleranceDeviations
 
 _logger = logging.getLogger("spicelib.SimAnalysis")
-
-from .tolerance_deviations import ToleranceDeviations, DeviationType, ComponentDeviation
-from ..process_callback import ProcessCallback
-from ...log.logfile_data import LogfileData
 
 
 class Montecarlo(ToleranceDeviations):
@@ -71,22 +71,31 @@ class Montecarlo(ToleranceDeviations):
         tol_uni_func = False
         tol_norm_func = False
         self.elements_analysed.clear()
-        for ref in self.get_components('*'):
-            val, dev = self.get_component_value_deviation_type(ref)  # get there present value
+        for ref in self.get_components("*"):
+            val, dev = self.get_component_value_deviation_type(
+                ref
+            )  # get there present value
             new_val = val
             if dev.typ == DeviationType.tolerance:
-                tolstr = ('%g' % dev.max_val).rstrip('0').rstrip('.')
-                if dev.distribution == 'uniform':
-                    new_val = "{utol(%s,%s)}" % (val, tolstr)  # calculate expression for new value
+                tolstr = ("%g" % dev.max_val).rstrip("0").rstrip(".")
+                if dev.distribution == "uniform":
+                    new_val = "{utol(%s,%s)}" % (
+                        val,
+                        tolstr,
+                    )  # calculate expression for new value
                     tol_uni_func = True
-                elif dev.distribution == 'normal':
+                elif dev.distribution == "normal":
                     new_val = "{ntol(%s,%s)}" % (val, tolstr)
                     tol_norm_func = True
             elif dev.typ == DeviationType.minmax:
-                if dev.distribution == 'uniform':
-                    new_val = "{urng(%s, %s,%s)}" % (val, dev.min_val, dev.max_val)  # calculate expression for new value
+                if dev.distribution == "uniform":
+                    new_val = "{urng(%s, %s,%s)}" % (
+                        val,
+                        dev.min_val,
+                        dev.max_val,
+                    )  # calculate expression for new value
                     min_max_uni_func = True
-                elif dev.distribution == 'normal':
+                elif dev.distribution == "normal":
                     new_val = "{nrng(%s,%s,%s)}" % (val, dev.min_val, dev.max_val)
                     min_max_norm_func = True
 
@@ -98,60 +107,86 @@ class Montecarlo(ToleranceDeviations):
             val, dev = self.get_parameter_value_deviation_type(param)
             new_val = val
             if dev.typ == DeviationType.tolerance:
-                if dev.distribution == 'uniform':
+                if dev.distribution == "uniform":
                     new_val = "{utol(%s,%g)}" % (val, dev.max_val)
                     tol_uni_func = True
-                elif dev.distribution == 'normal':
+                elif dev.distribution == "normal":
                     new_val = "{ntol(%g,%g)}" % (val, dev.max_val)
                     tol_norm_func = True
             elif dev.typ == DeviationType.minmax:
-                if dev.distribution == 'uniform':
-                    new_val = "{urng(%s,%g,%g)}" % (val, (dev.max_val+dev.min_val)/2, (dev.max_val-dev.min_val)/2)
+                if dev.distribution == "uniform":
+                    new_val = "{urng(%s,%g,%g)}" % (
+                        val,
+                        (dev.max_val + dev.min_val) / 2,
+                        (dev.max_val - dev.min_val) / 2,
+                    )
                     min_max_uni_func = True
-                elif dev.distribution == 'normal':
-                    new_val = "{nrng(%s,%g,%g)}" % (val, (dev.max_val+dev.min_val)/2, (dev.max_val-dev.min_val)/6)
+                elif dev.distribution == "normal":
+                    new_val = "{nrng(%s,%g,%g)}" % (
+                        val,
+                        (dev.max_val + dev.min_val) / 2,
+                        (dev.max_val - dev.min_val) / 6,
+                    )
                     min_max_norm_func = True
             else:
                 continue
             self.editor.set_parameter(param, new_val)
             self.elements_analysed.append(param)
 
-        if self.runner.simulator.__name__ == 'LTspice':
+        if self.runner.simulator.__name__ == "LTspice":
             if tol_uni_func:
-                self.editor.add_instruction(".func utol(nom,tol) if(run<0, nom, mc(nom,tol))")
+                self.editor.add_instruction(
+                    ".func utol(nom,tol) if(run<0, nom, mc(nom,tol))"
+                )
 
             if tol_norm_func:
-                self.editor.add_instruction(".func ntol(nom,tol) if(run<0, nom, nom*(1+gauss(tol/3)))")
+                self.editor.add_instruction(
+                    ".func ntol(nom,tol) if(run<0, nom, nom*(1+gauss(tol/3)))"
+                )
 
             if min_max_uni_func:
-                self.editor.add_instruction(".func urng(nom,mean,df2) if(run<0, nom, mean+flat(df2))")
+                self.editor.add_instruction(
+                    ".func urng(nom,mean,df2) if(run<0, nom, mean+flat(df2))"
+                )
 
             if min_max_norm_func:
-                self.editor.add_instruction(".func nrng(nom,mean,df6) if(run<0, nom, mean*(1+gauss(df6)))")
-        elif self.runner.simulator.__name__ == 'Qspice':
+                self.editor.add_instruction(
+                    ".func nrng(nom,mean,df6) if(run<0, nom, mean*(1+gauss(df6)))"
+                )
+        elif self.runner.simulator.__name__ == "Qspice":
             # if gauss function is needed
             #  => This is finally not needed because Qspice has a built-in gauss function (non-documented)
             # if tol_norm_func or min_max_norm_func:
             #   self.editor.add_instruction(".func random_not0() {(random()+1e-7)/(1+1e-7)}")
             #   self.editor.add_instruction(".func gauss(sigma) {sqrt(-2*ln(random_not0()))*cos(2*pi*random())*sigma}")
             if tol_uni_func:
-                self.editor.add_instruction(".func utol(nom,tol) {if(run<0, nom, nom*(1+tol*(2*random()-1)))}")
+                self.editor.add_instruction(
+                    ".func utol(nom,tol) {if(run<0, nom, nom*(1+tol*(2*random()-1)))}"
+                )
 
             if tol_norm_func:
-                self.editor.add_instruction(".func ntol(nom,tol) {if(run<0, nom, nom*(1+gauss(tol/3)))}")
+                self.editor.add_instruction(
+                    ".func ntol(nom,tol) {if(run<0, nom, nom*(1+gauss(tol/3)))}"
+                )
 
             if min_max_uni_func:
-                self.editor.add_instruction(".func urng(nom,mean,df2) if(run<0, nom, mean+(df2*(2*random()-1))")
+                self.editor.add_instruction(
+                    ".func urng(nom,mean,df2) if(run<0, nom, mean+(df2*(2*random()-1))"
+                )
 
             if min_max_norm_func:
-                self.editor.add_instruction(".func nrng(nom,mean,df6) if(run<0, nom, mean*(1+gauss(df6)))")
+                self.editor.add_instruction(
+                    ".func nrng(nom,mean,df6) if(run<0, nom, mean*(1+gauss(df6)))"
+                )
         else:
             _logger.warning("Simulator not supported for this method")
             raise NotImplementedError("Simulator not supported for this method")
 
-        self.last_run_number = kwargs.get('num_runs', self.last_run_number if self.last_run_number != 0 else 1000)
+        self.last_run_number = kwargs.get(
+            "num_runs", self.last_run_number if self.last_run_number != 0 else 1000
+        )
         self.editor.add_instruction(".step param run -1 %d 1" % self.last_run_number)
-        self.editor.set_parameter('run', -1)
+        self.editor.set_parameter("run", -1)
         self.testbench_prepared = True
 
     @staticmethod
@@ -159,41 +194,49 @@ class Montecarlo(ToleranceDeviations):
         """Returns a new value for the simulation"""
         new_val = value
         if dev.typ == DeviationType.tolerance:
-            if dev.distribution == 'uniform':
-                new_val = f"{random.Random().uniform(value * (1- dev.max_val), value * (1 + dev.max_val)):g}"
-            elif dev.distribution == 'normal':
-                new_val = f"{random.Random().gauss(value, dev.max_val / 3):g}"
+            if dev.distribution == "uniform":
+                new_val = random.Random().uniform(
+                    value * (1 - dev.max_val), value * (1 + dev.max_val)
+                )
+            elif dev.distribution == "normal":
+                new_val = random.Random().gauss(value, dev.max_val / 3)
         elif dev.typ == DeviationType.minmax:
-            if dev.distribution == 'uniform':
-                new_val = f"{random.Random().uniform(dev.min_val, dev.max_val):g}"
-            elif dev.distribution == 'normal':
-                new_val = f"{random.Random().gauss((dev.max_val + dev.min_val) / 2, (dev.max_val - dev.min_val) / 6):g}"
+            if dev.distribution == "uniform":
+                new_val = random.Random().uniform(dev.min_val, dev.max_val)
+            elif dev.distribution == "normal":
+                new_val = random.Random().gauss(
+                    (dev.max_val + dev.min_val) / 2, (dev.max_val - dev.min_val) / 6
+                )
         elif dev.typ == DeviationType.none:
             pass
         else:
             _logger.warning("Unknown deviation type")
         return new_val
 
-    def run_analysis(self,
-                     callback: Union[Type[ProcessCallback], Callable] = None,
-                     callback_args: Union[tuple, dict] = None,
-                     switches=None,
-                     timeout: float = None,
-                     num_runs: int = 1000,
-                     exe_log: bool = True
-                     ):
+    def run_analysis(
+        self,
+        callback: Optional[Union[Type[ProcessCallback], Callable]] = None,
+        callback_args: Optional[Union[tuple, dict]] = None,
+        switches=None,
+        timeout: Optional[float] = None,
+        exe_log: bool = True,
+        num_runs: int = 1000,
+    ):
         """This method runs the analysis without updating the netlist.
         It will update component values and parameters according to their deviation type and call the simulation.
         The advantage of this method is that it doesn't require adding random functions to the netlist.
-        The number of times the simulation is done is specified on the argument num_runs."""
+        The number of times the simulation is done is specified on the argument num_runs.
+        """
         self.elements_analysed.clear()
         self.clear_simulation_data()
         for run in range(num_runs):
             self._reset_netlist()  # reset the netlist
             self.play_instructions()  # play the instructions
             # Preparing the variation on components
-            for ref in self.get_components('*'):
-                val, dev = self.get_component_value_deviation_type(ref)  # get there present value
+            for ref in self.get_components("*"):
+                val, dev = self.get_component_value_deviation_type(
+                    ref
+                )  # get there present value
                 new_val = self._get_sim_value(val, dev)
                 if new_val != val:  # Only update the value if it has changed
                     self.editor.set_component_value(ref, new_val)  # update the value
@@ -204,16 +247,29 @@ class Montecarlo(ToleranceDeviations):
                 if new_val != val:  # Only update the value if it has changed
                     self.editor.set_parameter(param, new_val)
             # Run the simulation
-            rt = self.run(wait_resource=True,
-                          callback=callback, callback_args=callback_args,
-                          switches=switches, timeout=timeout, exe_log=exe_log)
+            # Handle optional parameters properly before passing to run
+            actual_callback = (
+                callback if callback is not None else lambda *args, **kwargs: None
+            )
+            actual_callback_args = callback_args if callback_args is not None else {}
+            actual_timeout = timeout if timeout is not None else float("inf")
+
+            rt = self.run(
+                wait_resource=True,
+                callback=actual_callback,
+                callback_args=actual_callback_args,
+                switches=switches,
+                timeout=actual_timeout,
+                exe_log=exe_log,
+            )
 
         self.runner.wait_completion()
         if callback is not None:
             callback_rets = []
             for rt in self.simulations:
-                callback_rets.append(rt.get_results())
-            self.simulation_results['callback_returns'] = callback_rets
+                if rt is not None:
+                    callback_rets.append(rt.get_results())
+            self.simulation_results["callback_returns"] = callback_rets
         self.analysis_executed = True
 
     def analyse_measurement(self, meas_name: str):
@@ -224,7 +280,9 @@ class Montecarlo(ToleranceDeviations):
         which is 99.7% of the data.
         """
         if not self.analysis_executed:
-            _logger.warning("The analysis was not executed. Please run the analysis before calling this method")
+            _logger.warning(
+                "The analysis was not executed. Please run the analysis before calling this method"
+            )
             return None
         log_data: LogfileData = self.read_logfiles()
         meas_data = log_data[meas_name]
