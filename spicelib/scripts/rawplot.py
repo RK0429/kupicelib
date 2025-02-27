d56b0d60d630b8ee30e78e64ef8eac8a87c347de
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-02-27 16:30:31 +0900

    Update rawplot.py

--- spicelib/scripts/rawplot.py
+++ spicelib/scripts/rawplot.py
@@ -23,29 +23,30 @@
 def main():
     """Uses matplotlib to plot the data in the raw file"""
     import sys
+
     import matplotlib
     import matplotlib.pyplot as plt
-
-    from numpy import abs as mag, angle as phase_np
-    from numpy import angle, arange
+    from numpy import abs as mag
+    from numpy import angle as phase_np
+    from numpy import arange
 
     from spicelib import RawRead
 
     def what_to_units(whattype):
         """Determines the unit to display on the plot Y axis"""
-        if 'voltage' in whattype:
-            return 'V'
-        if 'current' in whattype:
-            return 'A'
+        if "voltage" in whattype:
+            return "V"
+        if "current" in whattype:
+            return "A"
 
-    matplotlib.use('tkagg')
+    matplotlib.use("tkagg")
 
     if len(sys.argv) > 2:
         raw_filename = sys.argv[1]
         trace_names = sys.argv[2:]
     elif len(sys.argv) > 1:
         raw_filename = sys.argv[1]
-        trace_names = '*'  # All traces
+        trace_names = "*"  # All traces
     else:
         print("Usage: rawplot.py RAW_FILE TRACE_NAME")
         print("TRACE_NAME is the traces to plot")
@@ -55,7 +56,7 @@ def main():
     for param, value in LTR.raw_params.items():
         print("{}: {}{}".format(param, " " * (20 - len(param)), str(value).strip()))
 
-    if trace_names == '*':
+    if trace_names == "*":
 
         print("Reading all the traces in the raw file")
         trace_names = LTR.get_trace_names()
@@ -69,7 +70,7 @@ def main():
 
     n_axis = len(traces)
 
-    fig, axis_set = plt.subplots(nrows=n_axis, ncols=1, sharex='all')
+    fig, axis_set = plt.subplots(nrows=n_axis, ncols=1, sharex="all")
 
     if n_axis == 1:
         axis_set = [axis_set]  # Needs to have a list of axis
@@ -78,8 +79,8 @@ def main():
         ax = axis_set[i]
 
         ax.grid(True)
-        if 'log' in LTR.flags:
-            ax.set_xscale('log')
+        if "log" in LTR.flags:
+            ax.set_xscale("log")
         for step_i in steps_data:
             if LTR.axis:
                 x = LTR.get_axis(step_i)
@@ -87,18 +88,18 @@ def main():
                 x = arange(LTR.nPoints)
             y = traces[i].get_wave(step_i)
             label = f"{trace.name}:{steps_data[step_i]})"
-            if 'complex' in LTR.flags:
+            if "complex" in LTR.flags:
                 x = mag(x)
-                ax.set_yscale('log')
+                ax.set_yscale("log")
                 y = mag(y)
-                ax.yaxis.label.set_color('blue')
-                ax.set(ylabel=label+'(dB)')
+                ax.yaxis.label.set_color("blue")
+                ax.set(ylabel=label + "(dB)")
                 ax.plot(x, y)
                 ax_phase = ax.twinx()
                 y = phase_np(y, deg=True)
-                ax_phase.plot(x, y, color='red', linestyle='-.')
-                ax_phase.yaxis.label.set_color('red')
-                ax_phase.set(ylabel=label+'Phase (o)')
+                ax_phase.plot(x, y, color="red", linestyle="-.")
+                ax_phase.yaxis.label.set_color("red")
+                ax_phase.set(ylabel=label + "Phase (o)")
                 # title = f"{trace.name} Phase [deg]"
                 # ax.set_title(title)
             else:
@@ -108,8 +109,10 @@ def main():
                 # ax.set_title(title)
     fig.tight_layout()
     plt.show()
-'''
-'''
+
+
+"""
+"""
 # out = open("RAW_TEST_out_test1.txt", 'w')
 #
 # for step in LTR.get_steps():
