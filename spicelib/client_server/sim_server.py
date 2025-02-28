418a2ae55fd2e7add1ab05eb6c32438360e9a3e7
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-02-28 18:46:42 +0900

    Update sim_server.py

--- spicelib/client_server/sim_server.py
+++ spicelib/client_server/sim_server.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import io
+import logging
+import threading
+import uuid
+import zipfile
+from pathlib import Path
+
 # -------------------------------------------------------------------------------
 #
 #  ███████╗██████╗ ██╗ ██████╗███████╗██╗     ██╗██████╗
@@ -21,15 +28,10 @@
 from typing import Tuple
 from xmlrpc.client import Binary
 from xmlrpc.server import SimpleXMLRPCServer
-import logging
-_logger = logging.getLogger("spicelib.SimServer")
 
-import threading
-from pathlib import Path
-import zipfile
-import io
 from spicelib.client_server.srv_sim_runner import ServerSimRunner
-import uuid
+
+_logger = logging.getLogger("spicelib.SimServer")
 
 
 class SimServer(object):
@@ -50,25 +52,42 @@ class SimServer(object):
     :param port: The port where the server will listen for requests. Default is 9000
     """
 
-    def __init__(self, simulator, parallel_sims=4, output_folder='./temp', timeout: float = 300, port=9000,
-                 host='localhost'):
+    def __init__(
+        self,
+        simulator,
+        parallel_sims=4,
+        output_folder="./temp",
+        timeout: float = 300,
+        port=9000,
+        host="localhost",
+    ):
         self.output_folder = output_folder
-        self.simulation_manager = ServerSimRunner(parallel_sims=parallel_sims, timeout=timeout, verbose=False,
-                                                  output_folder=output_folder, simulator=simulator)
+        self.simulation_manager = ServerSimRunner(
+            parallel_sims=parallel_sims,
+            timeout=timeout,
+            verbose=False,
+            output_folder=output_folder,
+            simulator=simulator,
+        )
         self.server = SimpleXMLRPCServer(
-                (host, port),
-                # requestHandler=RequestHandler
+            (host, port),
+            # requestHandler=RequestHandler
         )
         self.server.register_introspection_functions()
         self.server.register_instance(self)
-        self.sessions = {}  # this will contain the session_id ids hashing their respective list of sim_tasks
+        self.sessions: dict[str, list[int]] = (
+            {}
+        )  # this will contain the session_id ids hashing their respective list of sim_tasks
         self.simulation_manager.start()
-        self.server_thread = threading.Thread(target=self.server.serve_forever, name="ServerThread")
+        self.server_thread = threading.Thread(
+            target=self.server.serve_forever, name="ServerThread"
+        )
         self.server_thread.start()
 
     def add_sources(self, session_id, zip_data) -> bool:
         """Add sources to the simulation. The sources are contained in a zip file will be added to the simulation
-        folder. Returns True if the sources were added and False if the session_id is not valid. """
+        folder. Returns True if the sources were added and False if the session_id is not valid.
+        """
         _logger.info(f"Server: Add sources {session_id}")
         if session_id not in self.sessions:
             return False  # This indicates that no job is started
@@ -77,7 +96,7 @@ class SimServer(object):
         _logger.debug("Server: Created the buffer")
         # Extract the contents of the zip file
         answer = False
-        with zipfile.ZipFile(zip_buffer, 'r') as zip_file:
+        with zipfile.ZipFile(zip_buffer, "r") as zip_file:
             for name in zip_file.namelist():
                 _logger.debug(f"Server: Writing {name} to zip file")
             if len(zip_file.namelist()) >= 0:
@@ -100,7 +119,9 @@ class SimServer(object):
     def start_session(self):
         """Returns an unique key that represents the session. It will be later used to sort the sim_tasks belonging
         to the session."""
-        session_id = str(uuid.uuid4())  # Needs to be a string, otherwise the rpc client can't handle it
+        session_id = str(
+            uuid.uuid4()
+        )  # Needs to be a string, otherwise the rpc client can't handle it
         _logger.info(f"Server: Starting session {session_id}")
         self.sessions[session_id] = []
         return session_id
@@ -121,10 +142,12 @@ class SimServer(object):
         ret = []
         for task_info in self.simulation_manager.completed_tasks:
             _logger.debug(task_info)
-            runno = task_info['runno']
+            runno = task_info["runno"]
             if runno in self.sessions[session_id]:
-                ret.append(runno)  # transfers the dictionary from the simulation_manager completed task
-                # to the return dictionary 
+                ret.append(
+                    runno
+                )  # transfers the dictionary from the simulation_manager completed task
+                # to the return dictionary
         _logger.debug(f"Server: Returning status {ret}")
         return ret
 
@@ -132,20 +155,20 @@ class SimServer(object):
         if runno in self.sessions[session_id]:
 
             for task_info in self.simulation_manager.completed_tasks:
-                if runno == task_info['runno']:
+                if runno == task_info["runno"]:
                     # Create a buffer to store the zip file in memory
-                    zip_file = task_info['zipfile']
-                    zip = zip_file.open('rb')
+                    zip_file = task_info["zipfile"]
+                    zip = zip_file.open("rb")
                     # Read the zip file from the buffer and send it to the server
                     zip_data = zip.read()
                     zip.close()
                     self.simulation_manager.erase_files_of_runno(runno)
                     return zip_file.name, Binary(zip_data)
 
-        return "", Binary(b'')  # Returns and empty data
+        return "", Binary(b"")  # Returns and empty data
 
     def close_session(self, session_id):
-        """Cleans all the pending sim_tasks with """
+        """Cleans all the pending sim_tasks with"""
         if session_id not in self.sessions:
             return False
         _logger.info(f"Closing session {session_id}")
@@ -163,4 +186,3 @@ class SimServer(object):
 
     def running(self):
         return self.simulation_manager.running()
-
