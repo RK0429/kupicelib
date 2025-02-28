fe4ae4113bc40b0ad66fc5bbeac559ad6399d027
Ryota Kobayashi <s.woods.m.29@gmail.com>
2025-02-28 18:49:26 +0900

    Update srv_sim_runner.py

--- spicelib/client_server/srv_sim_runner.py
+++ spicelib/client_server/srv_sim_runner.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+import logging
+
 # -------------------------------------------------------------------------------
 #
 #  ███████╗██████╗ ██╗ ██████╗███████╗██╗     ██╗██████╗
@@ -20,19 +22,19 @@
 # -------------------------------------------------------------------------------
 import threading
 import time
-from typing import Union, List, Dict, Any
-from pathlib import Path
 import zipfile
-import logging
-_logger = logging.getLogger("spicelib.ServerSimRunner")
+from pathlib import Path
+from typing import Any, Dict, List, Optional, Union
 
-from ..sim.sim_runner import SimRunner
 from ..editor.base_editor import BaseEditor
+from ..sim.sim_runner import SimRunner
+
+_logger = logging.getLogger("spicelib.ServerSimRunner")
 
 
 def zip_files(raw_filename: Path, log_filename: Path):
-    zip_filename = raw_filename.with_suffix('.zip')
-    with zipfile.ZipFile(zip_filename, 'w', zipfile.ZIP_DEFLATED) as zip_file:
+    zip_filename = raw_filename.with_suffix(".zip")
+    with zipfile.ZipFile(zip_filename, "w", zipfile.ZIP_DEFLATED) as zip_file:
         zip_file.write(raw_filename)
         zip_file.write(log_filename)
     return zip_filename
@@ -49,31 +51,48 @@ class ServerSimRunner(threading.Thread):
     by this class.
     """
 
-    def __init__(self, parallel_sims: int = 4, timeout: float = None, verbose=False,
-                 output_folder: str = None, simulator=None):
+    def __init__(
+        self,
+        parallel_sims: int = 4,
+        timeout: Optional[float] = None,
+        verbose=False,
+        output_folder: Optional[str] = None,
+        simulator=None,
+    ):
         super().__init__(name="SimManager")
-        self.runner = SimRunner(simulator=simulator, parallel_sims=parallel_sims, timeout=timeout,
-                                verbose=verbose, output_folder=output_folder)
-        self.completed_tasks: List[Dict[str, Any]] = []  # This is a list of dictionaries with the information of the completed tasks
+        # SimRunner expects a float for timeout, so use 600.0 as default if None is provided
+        sim_timeout = 600.0 if timeout is None else timeout
+        self.runner = SimRunner(
+            simulator=simulator,
+            parallel_sims=parallel_sims,
+            timeout=sim_timeout,
+            verbose=verbose,
+            output_folder=output_folder,
+        )
+        self.completed_tasks: List[Dict[str, Any]] = (
+            []
+        )  # This is a list of dictionaries with the information of the completed tasks
         self._stop = False
 
     def run(self) -> None:
-        """This function makes a direct manipulation of the structures of SimRunner. This option is """
+        """This function makes a direct manipulation of the structures of SimRunner. This option is"""
         while True:
             self.runner.update_completed()
             while len(self.runner.completed_tasks) > 0:
                 task = self.runner.completed_tasks.pop(0)
                 zip_filename = task.callback_return
-                self.completed_tasks.append({
-                    'runno': task.runno,
-                    'retcode': task.retcode,
-                    'circuit': task.netlist_file,
-                    'raw': task.raw_file,
-                    'log': task.log_file,
-                    'zipfile': zip_filename,
-                    'start': task.start_time,
-                    'stop': task.stop_time,
-                })
+                self.completed_tasks.append(
+                    {
+                        "runno": task.runno,
+                        "retcode": task.retcode,
+                        "circuit": task.netlist_file,
+                        "raw": task.raw_file,
+                        "log": task.log_file,
+                        "zipfile": zip_filename,
+                        "start": task.start_time,
+                        "stop": task.stop_time,
+                    }
+                )
                 _logger.debug(f"Task {task} is finished")
                 _logger.debug(self.completed_tasks[-1])
                 _logger.debug(len(self.completed_tasks))
@@ -84,7 +103,9 @@ class ServerSimRunner(threading.Thread):
         self.runner.wait_completion()
         self.runner.cleanup_files()  # Delete things that have been left behind
 
-    def add_simulation(self, netlist: Union[str, Path, BaseEditor], *, timeout: float = None) -> int:
+    def add_simulation(
+        self, netlist: Union[str, Path, BaseEditor], *, timeout: Optional[float] = None
+    ) -> int:
         """
         Adding a simulation to the list of simulations to be run. The function will return the runno of the simulation
         or -1 if the simulation could not be started.
@@ -94,7 +115,10 @@ class ServerSimRunner(threading.Thread):
         :return: The runno of the simulation or -1 if the simulation could not be started
         """
         _logger.debug(f"starting Simulation of {netlist}")
-        task = self.runner.run(netlist, wait_resource=True, timeout=timeout, callback=zip_files)
+        # SimRunner.run accepts Optional[float] for timeout, so we can pass it directly
+        task = self.runner.run(
+            netlist, wait_resource=True, timeout=timeout, callback=zip_files
+        )
         if task is None:
             _logger.error(f"Failed to start task {netlist}")
             return -1
@@ -104,7 +128,7 @@ class ServerSimRunner(threading.Thread):
 
     def _erase_files_and_info(self, pos):
         task = self.completed_tasks[pos]
-        for filename in ('circuit', 'log', 'raw', 'zipfile'):
+        for filename in ("circuit", "log", "raw", "zipfile"):
             f = task[filename]
             if f.exists():
                 _logger.info(f"deleting {f}")
@@ -115,7 +139,7 @@ class ServerSimRunner(threading.Thread):
         """Will delete all files related with a completed task. Will also delete information on the completed_tasks
         attribute."""
         for i, task_info in enumerate(self.completed_tasks):
-            if task_info['runno'] == runno:
+            if task_info["runno"] == runno:
                 self._erase_files_and_info(i)
                 break
 
